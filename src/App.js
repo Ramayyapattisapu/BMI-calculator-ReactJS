0046bcf20b38acf2a078b60eaf28d1dee07ac240
Ramayyapattisapu <ramayyapattisapu@gmail.com>
2023-12-23 15:20:18 +0530

    modified weight in kgs and display

--- src/App.js
+++ src/App.js
@@ -1,30 +1,56 @@
 import React, { useState } from "react";
 import "./index.css";
 
+// Helper function to check if a value is numeric
+const isNumeric = (value) => {
+  return !isNaN(parseFloat(value)) && isFinite(value);
+};
+
 function App() {
   // state
-  const [weight, setWeight] = useState(0);
-  const [height, setHeight] = useState(0);
+  const [weight, setWeight] = useState();
+  const [height, setHeight] = useState();
   const [bmi, setBmi] = useState("");
   const [message, setMessage] = useState("");
+  const [submitted, setSubmitted] = useState(false);
 
   let calBmi = (event) => {
     event.preventDefault();
-    if (weight === 0 || height === 0) {
-      alert("Please enter valid height and weight");
-    } else {
-      let bmi = (weight / (height * height)) * 703;
-      setBmi(bmi.toFixed(1));
 
+    if (!isNumeric(weight) || !isNumeric(height)) {
       // alert message
-      if (bmi < 25) {
-        setMessage("You are underweight");
-      } else if (bmi >= 25 && bmi < 30) {
-        setMessage("You are healthy maiintain the same");
-      } else {
-        setMessage("You are overweight");
-      }
+      alert("Please enter valid height and weight");
+      return;
     }
+
+    // Convert weight to kilograms
+    const weightInKg = weight;
+
+    // Convert height to meters
+    const heightInMeters = height / 100; // Convert height from cm to meters
+
+    let calculatedBmi = weightInKg / (heightInMeters * heightInMeters);
+
+    setBmi(calculatedBmi.toFixed(1));
+
+    // set message
+    if (calculatedBmi < 18.5) {
+      setMessage("You are underweight");
+    } else if (calculatedBmi >= 18.5 && calculatedBmi < 24.9) {
+      setMessage("You are healthy maintain the same");
+    } else {
+      setMessage("You are overweight");
+    }
+
+    setSubmitted(true);
+  };
+
+  let reload = () => {
+    setWeight("");
+    setHeight("");
+    setBmi("");
+    setMessage("");
+    setSubmitted(false);
   };
 
   // show image based on bmi calculation
@@ -32,18 +58,14 @@ function App() {
   if (bmi < 1) {
     imgSrc = null;
   } else {
-    if (bmi < 25) {
+    if (bmi < 18.5) {
       imgSrc = require("../src/assets/underweight.png");
-    } else if (bmi >= 25 && bmi < 30) {
+    } else if (bmi >= 18.5 && bmi < 24.9) {
       imgSrc = require("../src/assets/healthy.png");
     } else {
       imgSrc = require("../src/assets/overweight.png");
     }
   }
-
-  let reload = () => {
-    window.location.reload();
-  };
 
   return (
     <div className="app">
@@ -51,14 +73,14 @@ function App() {
         <h2 className="center">BMI CALCULATOR</h2>
         <form onSubmit={calBmi}>
           <div>
-            <label>Weight (lbs)</label>
+            <label>Weight (kg)</label>
             <input
               value={weight}
               onChange={(event) => setWeight(event.target.value)}
             />
           </div>
           <div>
-            <label>Height (in)</label>
+            <label>Height (cm)</label>
             <input
               value={height}
               onChange={(event) => setHeight(event.target.value)}
@@ -68,19 +90,22 @@ function App() {
             <button className="btn" type="submit">
               Submit
             </button>
-            <button className="btn btn-outline" onClick={reload} type="submit">
+            <button className="btn btn-outline" onClick={reload} type="button">
               Reload
             </button>
           </div>
         </form>
-        <div className="center">
-          <h3> Your BMI is: {bmi}</h3>
-          <p>{message}</p>
-        </div>
-
-        <div className="img-container">
-          <img src={imgSrc} alt=""></img>
-        </div>
+        {submitted && (
+          <div className="center">
+            <h3>Your BMI is: {bmi}</h3>
+            <p>{message}</p>
+          </div>
+        )}
+        {submitted && bmi && imgSrc && (
+          <div className="img-container">
+            <img src={imgSrc} alt="" />
+          </div>
+        )}
       </div>
     </div>
   );
